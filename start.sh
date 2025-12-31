790484b1eedd803ffd0bc18f8ce014ea8929b9f5
FEDOKH <158352652+F3D0KH@users.noreply.github.com>
2025-12-31 17:45:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+echo >> "############################################"
+echo >> "#                                          #"
+echo >> "# Rool to make your work on stemdeck easier!"
+echo >> "#             by F3D0KH                    #"
+echo >> "############################################"
