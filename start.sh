139822011a7a9cf6d02c0153ec7472ad504daa6c
FEDOKH <158352652+F3D0KH@users.noreply.github.com>
2025-12-31 18:04:02 +0300

    Base functions start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo >> "############################################"
 echo >> "#                                          #"
-echo >> "# Tool to make your work on stemdeck easier!"
+echo >> "# Tool to make your work on steamdeck easier!"
 echo >> "#             by F3D0KH                    #"
 echo >> "############################################"
+
+echo >> "Choose your option:"
+echo >> "1) Readonly disable"
+#echo >> "2) "
+read answer
+if ($answer == 1) {
+  sudo steamos-readonly disable
+}
+#if ()
