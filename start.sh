a23b88000a93fd1235a6a3f754955f09589c2692
FEDOKH <158352652+F3D0KH@users.noreply.github.com>
2026-01-15 21:58:20 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ read answer
 if [ "$answer" == 1 ]; then
     sudo steamos-readonly disable
 fi
-if [ "$answer" == 2]; then
+if [ "$answer" == 2 ]; then
     sudo pacman -Suy
 fi
 if [ "$answer" == 3 ]; then
@@ -29,12 +29,13 @@ if [ "$answer" == 4 ]; then
     sudo pacman -Sy tailscale
 fi
 if [ "$answer" == 5 ]; then
-    read -p "Please write correct path to existing directory(or if you want to install not in the current directory please type " "):" path
-    if [ "$path" == " " ] then
+    read -p "Please write correct path to existing directory(or if you want to install not in the current directory please type ENTER):" path
+    if [ -z "$path" ]; then
         git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git 
     else
         git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git $path
     fi
+fi
 if [ "$answer" == 6 ]; then
     sudo systemctl start wg-quick@wg0.service
 fi
