a7233372c7e4c91e353d2d22832f5f212c1eab66
FEDOKH <158352652+F3D0KH@users.noreply.github.com>
2026-01-15 21:53:14 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,25 +16,25 @@ echo "1) Readonly disable                          2) Update all pkg"
 echo "3) Fuck you pacman!(Switch SigLevel)         4) Install tailscale"
 echo "5) Download latest version zapret(linux)     6) start WG"
 read answer
-if ["$answer" == 1]; then
+if [ "$answer" == 1 ]; then
     sudo steamos-readonly disable
 fi
-if ["$answer" == 2]; then
+if [ "$answer" == 2]; then
     sudo pacman -Suy
 fi
-if ["$answer" == 3]; then
+if [ "$answer" == 3 ]; then
     asudo sed -i '42s/.*/SigLevel = Never/' /etc/pacman.conf
 fi
-if ["$answer" == 4]; then
+if [ "$answer" == 4 ]; then
     sudo pacman -Sy tailscale
 fi
-if [$answer == 5]; then
+if [ "$answer" == 5 ]; then
     read -p "Please write correct path to existing directory(or if you want to install not in the current directory please type " "):" path
-    if [$path == " "] then
+    if [ "$path" == " " ] then
         git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git 
     else
         git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git $path
     fi
-if [$answer == 6]; then
+if [ "$answer" == 6 ]; then
     sudo systemctl start wg-quick@wg0.service
 fi
